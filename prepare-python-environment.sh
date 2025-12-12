5e3a73d6bc23c7a787b114e39b9fce8d5cdb7105
Johnny Cederholm <johnny.k.cederholm@gmail.com>
2025-12-12 22:40:45 +0100

    Add playbook to setup Python venv on host

---
+++ prepare-python-environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source .venv/bin/activate
+read -p "User to run playbook as: " ANSIBLE_USER
+ansible-playbook \
+    --user ${ANSIBLE_USER} \
+    --ask-pass \
+    --ask-become-pass \
+    --inventory inventory.yml \
+    --limit vm \
+    python-environment.yml
